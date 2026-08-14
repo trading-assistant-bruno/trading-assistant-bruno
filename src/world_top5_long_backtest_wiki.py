910d2a6de85cc753259b68afae80f0d273523c56
trading-assistant-bruno <bruno.swingtrading@gmail.com>
2026-08-14 10:29:41 +0200

    Use direct Wikipedia source for long backtest

---
+++ src/world_top5_long_backtest_wiki.py
@@ -0,0 +1,6 @@
+import world_top5_long_backtest as backtest
+
+backtest.FT_MIRROR = 'https://en.wikipedia.org/wiki/List_of_public_corporations_by_market_capitalization'
+
+if __name__ == '__main__':
+    backtest.main()
