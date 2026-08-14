c02f3df28b645d331730c0bb5a4c14603587779a
trading-assistant-bruno <bruno.swingtrading@gmail.com>
2026-08-14 10:32:06 +0200

    Use HandWiki FT archive for long backtest

--- src/world_top5_long_backtest_wiki.py
+++ src/world_top5_long_backtest_wiki.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 import world_top5_long_backtest as backtest
 
-backtest.FT_MIRROR = 'https://en.wikipedia.org/wiki/List_of_public_corporations_by_market_capitalization'
+backtest.FT_MIRROR = 'https://handwiki.org/wiki/Finance:List_of_public_corporations_by_market_capitalization'
 
 
 def parse_ft_year_wiki(year: int):
